c04ea13aa7021599fcfdfd2a5b7dd84775078403
killsten <inspiration.ty@gmail.com>
2019-09-27 21:28:40 +0300

    GitHub Finder

---
+++ src/js/github.js
@@ -0,0 +1,27 @@
+
+class Github {
+    constructor() {
+        this.client_id = 'aceb27b5a9c959957a0a';
+        this.client_secret = '8376af0a5858b47f8dee9e146d6d89e8e3214225'; 
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=$
+        {this.client_secret}`)
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?
+        per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
+
+
